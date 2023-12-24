50b3eeb07814dcdc32c37f2276ec88ab3ddebbaf
Rodrigo <rtf.franca@gmail.com>
2023-12-24 05:11:41 -0300

    Building the App Layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./Sidebar.module.css";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by Rodrigo França
+        </p>
+      </footer>
+    </div>
+  );
+}
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+import styles from './AppLayout.module.css'
+import Map from "../components/Map";
 
 export default function AppLayout() {
-  return <div>
-    <AppNav />
+  return <div className={styles.app}>
+   <Sidebar />
+   <Map />
   </div>;
 }
