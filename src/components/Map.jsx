1dfdb5930f481568c8c8b32ce9f4afdb9e3427d2
Rodrigo <rtf.franca@gmail.com>
2023-12-29 00:56:45 -0300

    Programmatic navigation with useNavigate

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export default function Map() {
+  const navigate = useNavigate()
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get('lng')
-  return <div className={styles.mapContainer}>
+  return <div className={styles.mapContainer} onClick={() => {
+    navigate('form')
+  }}>
     <h1>Map</h1>
     <h1>
       Position: {lat} , {lng}
